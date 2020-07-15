e9b2763f24e9cd7c544498334dde21794a141506
spot-wellbeing <spotwellbeing@gmail.com>
2020-07-15 12:35:28 +0100

    social splash image

--- components/Meta/index.js
+++ components/Meta/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Meta = () => (
   <>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta property="og:image" content="/img/social-splash.jpg" />
     <link rel="icon" type="image/png" href="/img/spot-wellbeing-favicon.png" />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;500&display=swap" rel="stylesheet" />
     <script async src="https://cdn.ampproject.org/v0.js" />
